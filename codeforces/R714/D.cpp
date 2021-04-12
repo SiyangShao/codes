b9d7200117ae9ca5c49aebaf45fef4b16e1b8bc7
SiyangShao <siyang.shao@outlook.com>
2021-04-12 22:29:58 +0800

    R714补题

--- codeforces/R714/D.cpp
+++ codeforces/R714/D.cpp
@@ -1,56 +1,42 @@
 #include <bits/stdc++.h>
 #define int long long
-typedef long long ll;
 using namespace std;
-int _;
-int n, a[200010], p;
+struct node {
+	int n, i;
+} a[200010];
+int _, p, n, b[200010], vis[200010];
+bool cmp(node x, node y) { return x.n < y.n; }
 signed main() {
 	ios::sync_with_stdio(false);
-	cin.tie(0);
-	cout.tie(0);
+	cin.tie(0), cout.tie(0);
 	cin >> _;
 	while (_--) {
+		memset(vis, 0, sizeof(vis));
 		cin >> n >> p;
-		int ans = 0, le = 0;
-		// vector<pair<int,int>>q;
-		int qr = 0;
-		for (int i = 1; i <= n; i++)
-			cin >> a[i];
-		for (int i = 2; i <= n; i++) {
-			if (max(a[i], a[i - 1]) % min(a[i], a[i - 1]) == 0) {
-				int tmp = min(a[i], a[i - 1]);
-				int r = i;
-				for (r = i; r <= n; r++) {
-					if (a[r] % tmp == 0) {
-					} else if (tmp % a[r] == 0) {
-						tmp = a[r];
-					} else {
-						r--;
-						break;
-					}
-				}
-				int l = i - 1;
-				for (l = i - 1; l >= 1; l--) {
-					if (a[l] % tmp == 0) {
-					} else if (tmp % a[l] == 0) {
-						tmp = a[l];
-					} else {
-						l++;
-						break;
-					}
-				}
-				l = max(ll(1), l);
-				r = min(r, n);
-				i = r + 1;
-				// q.push_back(make_pair(l,r));
-				ans++;
-				le += r - l + 1;
-				qr += (r - l) * min(tmp, p);
-			}
+		for (int i = 1; i <= n; i++) {
+			cin >> b[i];
+			a[i].n = b[i];
+			a[i].i = i;
 		}
-		ans = n - le + ans;
-		// cout<<ans<<"\n";
-		qr += (ans - 1) * p;
-		cout << qr << "\n";
+		sort(a + 1, a + 1 + n, cmp);
+		int ans = 0, rest = n - 1;
+		for (int i = 1; i <= n; i++) {
+			if (a[i].n > p)
+				break;
+			if (vis[a[i].i])
+				continue;
+			int l = a[i].i, r = a[i].i;
+			while (l > 1 && !vis[l] && (b[l - 1] % a[i].n == 0))
+				l--;
+			while (r < n && !vis[r] && (b[r + 1] % a[i].n == 0))
+				r++;
+			for (int i = l; i <= r; i++) {
+				vis[i] = 1;
+			}
+			rest -= r - l;
+			ans += (r - l) * a[i].n;
+		}
+		ans += rest * p;
+		cout << ans << "\n";
 	}
 }
