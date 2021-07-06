eb764763b077dcac222ec47b1e3338aed773f2c6
shaosy <siyang.shao@outlook.com>
2021-07-06 13:53:27 +0800

    test

---
+++ codeforces/R729/B.cpp
@@ -0,0 +1,25 @@
+#include <bits/stdc++.h>
+typedef long long ll;
+using namespace std;
+
+int main(){
+  ll T;
+  ll n, a, b, base;
+  bool flag;
+  scanf("%lld", &T);
+  while (T--) {
+    scanf("%lld%lld%lld", &n, &a, &b);
+    base = 1;
+    flag = false;
+    for (ll i = 1; i <= 30 && base <= n; i++, base *= a) {
+      if ((n - base) % b == 0) {
+        flag = true;
+        break;
+      }
+    }
+    if (flag)
+      printf("Yes\n");
+    else
+      printf("No\n");
+  }
+}
