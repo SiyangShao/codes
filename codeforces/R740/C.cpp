02da62fc98b1af8418e374afac3f09c00aafaae9
shaosy <siyang.shao@outlook.com>
2021-09-06 10:13:57 +0800

    memset开销过大

--- codeforces/R740/C.cpp
+++ codeforces/R740/C.cpp
@@ -3,18 +3,17 @@
 using namespace std;
 int _, n;
 struct node {
-	int power, i, number;
+	int power, number;
 } k[100010];
 bool cmp(node a, node b) { return a.power < b.power; }
 signed main() {
 	scanf("%lld", &_);
 	while (_--) {
-        memset(k,0,sizeof(k));
 		scanf("%lld", &n);
 		for (int i = 1, tmp; i <= n; ++i) {
 			scanf("%lld", &tmp);
-			k[i].i = i;
 			k[i].number = tmp;
+			k[i].power=0;
 			for (int j = 0, q; j < tmp; ++j) {
 				scanf("%lld", &q);
 				if (k[i].power > q - j) {
@@ -26,10 +25,16 @@ signed main() {
 		}
 		sort(k + 1, k + 1 + n, cmp);
 		int ans = 0, now = 0;
+		// for(int i = 1 ; i<=n ; ++i){
+		// 	printf("%lld:%lld ",k[i].power,k[i].number);
+		// }
+		// puts("");
 		for (int i = 1; i <= n; ++i) {
 			if (now < k[i].power) {
 				ans += k[i].power - now;
 				now = k[i].power + k[i].number;
+			}else{
+				now+=k[i].number;
 			}
 		}
 		printf("%lld\n", ans);
