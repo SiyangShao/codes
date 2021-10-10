580b84f1b8371c7312129ad35eb089082253536b
shaosy <siyang.shao@outlook.com>
2021-10-10 21:51:56 +0800

    水

---
+++ codeforces/R747/A.cpp
@@ -0,0 +1,11 @@
+#include <bits/stdc++.h>
+using namespace std;
+typedef long long ll;
+int main(){
+    ll n,t,l,r;
+    scanf("%lld",&t);
+    while(t--){
+        scanf("%lld",&n);
+        printf("%lld %lld\n",-n+1,n);
+    }
+}
