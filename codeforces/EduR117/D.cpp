b80edbbb9716468ccfa53155df9c16d0cacd04e9
siyang shao <siyang.shao@outlook.com>
2021-11-29 18:11:54 +0800

    f

--- codeforces/EduR117/D.cpp
+++ codeforces/EduR117/D.cpp
@@ -2,18 +2,20 @@
 using namespace std;
 typedef long long ll;
 bool gcd(ll a, ll b, ll x) {
-	if (a < b) {
+	if (a < x && b < x)
+		return 0;
+	if (a == x || b == x)
+		return 1;
+	if ((a == b) || (a == 0) || (b == 0))
+		return 0;
+	if (a < b)
 		swap(a, b);
-	}
-	if (a < x && b < x) {
-		return false;
-	} else if (a == x || b == x) {
-		return true;
-	} else if (x % b == a % b) {
-		return true;
-	} else {
-		return gcd(b, a % b, x);
-	}
+	if ((a - x) % b == 0)
+		return 1;
+	if (a % b == 0)
+		return x % b == 0;
+	a = a % b + max(x - x % b, b);
+	return gcd(a - b, b, x);
 }
 int main() {
 	ios::sync_with_stdio(false);
