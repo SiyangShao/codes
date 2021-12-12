67cae0ed05108a2fb5c084a97ce22510e5ff7a15
siyang shao <siyang.shao@outlook.com>
2021-12-12 16:03:29 +0800

    combinatorics

--- codeforces/R758/D.cpp
+++ codeforces/R758/D.cpp
@@ -1,77 +1,62 @@
 #include <bits/stdc++.h>
 using namespace std;
 typedef long long ll;
-const ll mod = 998244353;
-ll W_l, W_r, B_l, B_r, C_l, C_r;
-inline ll cal(ll m_start, ll m_end) {
-	ll L = 1, R = 1;
-	for (ll i = 1; i <= C_l; ++i) {
-		L = L * i % mod;
-	}
-	for (ll i = 1; i <= C_r; ++i) {
-		R = R * i % mod;
-	}
-	ll M = 1, N_M = 1, T_M = 1;
-	for (ll i = 1; i < m_start; ++i) {
-		M = M * i % mod;
-	}
-	for (ll i = 1; i <= C_l - m_start; ++i) {
-		N_M = N_M * i % mod;
-	}
-	for (ll i = 1; i <= C_r - m_start; ++i) {
-		T_M = T_M * i % mod;
-	}
-	ll ans = 0;
-	for (ll i = m_start; i <= m_end; ++i) {
-		if (i)
-			M = M * i % mod;
-		cout << L << " " << R << " " << M << " " << T_M << " " << N_M <<
-		"\n";
-		ans += ((L * R) % mod) / ((M * M % mod) * (T_M * N_M % mod));
-		ans %= mod;
-		if (N_M < C_l - i) {
-			N_M += mod;
-		}
-		if (T_M < C_r - i) {
-			T_M += mod;
-		}
-		if (!C_l - i)
-			N_M = 1;
-		else
-			N_M /= (C_l - i);
-		if (!C_r - i)
-			T_M = 1;
-		else
-			T_M /= (C_r - i);
-	}
-	return ans;
+const int mod = 998244353;
+
+int n, num[2], WW, BB, EMPTY, WB, BW;
+
+inline void add(char c) {
+	if (c == 'W')
+		num[0]++;
+	else if (c == 'B')
+		num[1]++;
+}
+int qpow(ll a, int b) {
+	int r = 1;
+	for (; b; b >>= 1, a = a * a % mod)
+		if (b & 1)
+			r = a * r % mod;
+	return r;
 }
 
 int main() {
-	// ios::sync_with_stdio(false);
-	// cin.tie(0), cout.tie(0);
-	ll n;
-	string tmp;
+	ios::sync_with_stdio(false);
+	cin.tie(0);
 	cin >> n;
-	W_l = 0, W_r = 0, B_l = 0, B_r = 0, C_l = 0, C_r = 0;
-	while (n--) {
-		cin >> tmp;
-		if (tmp[0] == '?') {
-			C_l++;
-		} else if (tmp[0] == 'B') {
-			B_l++;
-		} else if (tmp[0] == 'W') {
-			W_l++;
-		}
-		if (tmp[1] == '?') {
-			C_r++;
-		} else if (tmp[1] == 'B') {
-			B_r++;
-		} else if (tmp[1] == 'W') {
-			W_r++;
-		}
+	string s;
+	for (int i = 1; i <= n; ++i) {
+		cin >> s;
+		if (s == "WW")
+			WW = 1;
+		else if (s == "BB")
+			BB = 1;
+		else if (s == "??")
+			EMPTY++;
+		else if (s[0] == 'W' || s[1] == 'B')
+			WB = 1;
+		else if (s[0] == 'B' || s[1] == 'W')
+			BW = 1;
+		add(s[0]), add(s[1]);
 	}
-	ll mi = max(B_r - W_l, 0ll);
-	ll ma = min(mi + C_l, mi + C_r - max(B_l - W_r, 0ll));
-	cout << cal(mi, ma);
+	if (num[0] > n || num[1] > n)
+		return puts("0");
+	ll ans = 1;
+	int f = WW + BB;
+	for (int i = 1; i <= n - num[0]; ++i) {
+		ans = ans * (2 * n - num[0] - num[1] - i + 1) % mod * qpow(i, mod - 2) %
+			  mod;
+	}
+	// cout<<ans<<'\n';
+	if (f == 0) {
+		if (WB + BW == 2)
+			ans -= qpow(2, EMPTY);
+		else if (WB + BW == 1)
+			ans = ans - qpow(2, EMPTY) + 1;
+		else
+			ans = ans - qpow(2, EMPTY) + 2;
+		if (ans < 0)
+			ans += mod;
+	}
+	cout << ans << '\n';
+	return 0;
 }
