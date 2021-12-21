8c4488e7f80f137bf22367283b8c9c24120fde09
siyang shao <siyang.shao@outlook.com>
2021-12-21 16:56:34 +0800

    Changing

--- codeforces/R762/B.cpp
+++ codeforces/R762/B.cpp
@@ -5,9 +5,19 @@ const double eps = 1e-8;
 inline void solve() {
 	ll n;
 	cin >> n;
-	cout << ll(pow(n, 1.0 / 2) + eps) + ll(pow(n, 1.0 / 3) + eps) -
-				ll(pow(n, 1.0 / 6) + eps)
-		 << "\n";
+	ll a = pow(n, 1.0 / 2);
+	ll b = pow(n, 1.0 / 3);
+	ll c = pow(n, 1.0 / 6);
+	if ((a + 1) * (a + 1) <= n) {
+		a++;
+	}
+	if ((b + 1) * (b + 1) * (b + 1) <= n) {
+		b++;
+	}
+	if ((c + 1) * (c + 1) * (c + 1) * (c + 1) * (c + 1) * (c + 1) <= n) {
+		c++;
+	}
+	cout << a + b - c << "\n";
 }
 int main() {
 	ios::sync_with_stdio(false);
--- codeforces/R762/D.cpp
+++ codeforces/R762/D.cpp
@@ -5,10 +5,12 @@ inline void solve() {
 	int m, n;
 	cin >> m >> n;
 	memset(mp, 0, sizeof(mp));
+	vector<vector<int>> E(m);
 	int minu = 1e9 + 7;
 	for (int i = 1; i <= m; ++i) {
 		for (int j = 1, u; j <= n; ++j) {
 			cin >> u;
+			E[i - 1].push_back(u);
 			minu = min(minu, u);
 			mp[j].push_back({u, i});
 		}
@@ -41,13 +43,29 @@ inline void solve() {
 	}
 	int re = 0;
 	for (int i = 1; i <= n; ++i) {
-		for (int j = 2; j <= m; ++j) {
+		for (int j = 1; j < m; ++j) {
 			if (cnt[mp[i][j].second]) {
 				if (mp[i][j].first > minu) {
 					cout << minu << "\n";
 					return;
 				} else {
 					re = max(re, mp[i][j].first);
+					break;
+				}
+			} else {
+				bool flag = false;
+				int tmp = mp[i][j].second, tim = 0, lea = mp[i][j].first;
+				for (int k = 0; k < n; ++k) {
+					if (E[tmp-1][k] >= lea) {
+						tim++;
+					}
+					if (tim >= 2) {
+						flag = true;
+						re = max(re, mp[i][j].first);
+						break;
+					}
+				}
+				if (flag) {
 					break;
 				}
 			}
