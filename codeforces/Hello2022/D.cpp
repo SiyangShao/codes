8ef1dd9cfcff74e6b2294c847a6da9437c45aa10
siyang shao <siyang.shao@outlook.com>
2022-01-04 15:03:48 +0800

    ...

--- codeforces/Hello2022/D.cpp
+++ codeforces/Hello2022/D.cpp
@@ -3,12 +3,11 @@ using namespace std;
 #define int ll
 typedef long long ll;
 int n;
-vector<vector<int>> mp, L, C;
+vector<vector<int>> mp;
 void solve() {
 	cin >> n;
 	mp.clear();
 	mp.resize(n + n + 1, vector<int>(n + n + 1, 0));
-	L.clear(), C.clear();
 	int ans = 0;
 	for (int i = 1; i <= n + n; ++i) {
 		for (int j = 1; j <= n + n; ++j) {
@@ -18,32 +17,11 @@ void solve() {
 			}
 		}
 	}
-	int MINIUM = -1;
-	L = mp;
-	C = mp;
-	for (int i = 1; i <= n + n; ++i) {
-		for (int j = 1; j <= n + n; ++j) {
-			L[i][j] += L[i - 1][j];
-			C[i][j] += C[i][j - 1];
-		}
-	}
-
-	for (int i = n + 1; i <= n + n; ++i) {
-		for (int j = 1; j <= n; ++j) {
-			int tmp = min(C[i][j] - C[i][0], C[i][n] - C[i][j - 1]) +
-					  min(L[i][j] - L[n][j], L[n + n][j] - L[i - 1][j]) -
-					  mp[i][j];
-			MINIUM = MINIUM == -1 ? tmp : min(MINIUM, tmp);
-		}
-	}
-	for (int i = 1; i <= n; ++i) {
-		for (int j = n + 1; j <= n + n; ++j) {
-			int tmp = min(C[i][j] - C[i][n - 1], C[i][n + n] - C[i][j - 1]) +
-					  min(L[i][j] - L[0][j], L[n][j] - L[i - 1][j]) - mp[i][j];
-			MINIUM = MINIUM == -1 ? tmp : min(MINIUM, tmp);
-		}
-	}
-	cout << MINIUM + ans << "\n";
+	int MINIUM = 0x3f3f3f3f3f3f3f3f;
+	MINIUM = min(
+		min(min(mp[n + 1][n], mp[n + 1][1]), min(mp[n + n][1], mp[n + n][n])),
+		min(min(mp[n][n + 1], mp[1][n + 1]), min(mp[1][n + n], mp[n][n + n])));
+	cout << ans + MINIUM << "\n";
 }
 signed main() {
 	ios::sync_with_stdio(false);
