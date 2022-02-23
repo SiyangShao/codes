193191e5e976b5bc6878492e809cef31daee17cd
siyang shao <siyang.shao@outlook.com>
2022-02-23 18:12:32 +0800

    upd

---
+++ codeforces/R773/A.cpp
@@ -0,0 +1,15 @@
+#include <bits/stdc++.h>
+using namespace std;
+typedef long long ll;
+void solve(){
+    
+}
+int main(){
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr),cout.tie(nullptr);
+    int _ = 1;
+    cin>>_;
+    while(_--){
+        solve();
+    }
+}
