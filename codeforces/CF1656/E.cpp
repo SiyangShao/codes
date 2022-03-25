de3fff3bb903572e2162bc37e3c583b0843c55fd
siyang shao <siyang.shao@outlook.com>
2022-03-25 14:38:43 +0800

    WOW

--- codeforces/CF1656/E.cpp
+++ codeforces/CF1656/E.cpp
@@ -2,7 +2,30 @@
 using namespace std;
 using ll = long long;
 auto solve() {
-    
+    ll n;
+    cin >> n;
+    vector<vector<ll>> E(n);
+    for (ll i = 1, u, v; i < n; ++i) {
+        cin >> u >> v;
+        u--, v--;
+        E[u].push_back(v);
+        E[v].push_back(u);
+    }
+    vector<ll> ans(n);
+    auto dfs = [&](auto self, auto u, auto fa, auto x) -> void {
+        ans[u] = x;
+        for (auto v : E[u]) {
+            if (v == fa)
+                continue;
+            self(self, v, u, x == 0 ? 1 : -x);
+            ans[u] -= x == 0 ? 1LL : -x;
+        }
+    };
+    dfs(dfs, 0, -1, 0);
+    for (auto i : ans) {
+        cout << i << " ";
+    }
+    cout << "\n";
 }
 auto main() -> int {
     ios::sync_with_stdio(false);
