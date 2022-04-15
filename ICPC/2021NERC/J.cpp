219e43e7cc645754c6e122df085d7aec8abdb5db
siyang shao <siyang.shao@outlook.com>
2022-04-15 13:38:53 +0800

    dp on tree

--- ICPC/2021NERC/J.cpp
+++ ICPC/2021NERC/J.cpp
@@ -1,15 +1,62 @@
 #include <bits/stdc++.h>
 using namespace std;
 using ll = long long;
-constexpr ll MOD = 998244353;
+int n;
+vector<vector<ll>> mp, dp, fro;
+vector<int> fa;
+auto subtree(int l, int r) {
+    if (l > r)
+        return 0LL;
+    if (dp[l][r] != -1) {
+        return dp[l][r];
+    }
+    ll cnt = LONG_LONG_MAX;
+    for (ll k = l; k <= r; ++k) {
+        auto tmp = subtree(l, k - 1) + subtree(k + 1, r);
+        if (tmp < cnt) {
+            cnt = tmp;
+            fro[l][r] = k;
+        }
+    }
+    for (ll i = l; i <= r; ++i) {
+        cnt += mp[i][n] - (mp[i][r] - mp[i][l - 1]);
+    }
+    return dp[l][r] = cnt;
+}
+auto build(int l, int r, int up) {
+    if (l > r)
+        return;
+    if (l == r) {
+        fa[l] = up;
+        return;
+    }
+    auto k = fro[l][r];
+    // cout << l << " " << r << " " << k << endl;
+    fa[k] = up;
+    build(l, k - 1, k);
+    build(k + 1, r, k);
+}
 auto solve() {
-    int n;
     cin >> n;
-    vector<vector<ll>> mp(n + 1, vector<ll>(n + 1, 0));
+    fa.assign(n + 1, 0);
+    mp.assign(n + 1, vector<ll>(n + 1, 0));
+    dp.assign(n + 1, vector<ll>(n + 1, -1));
+    fro.assign(n + 1, vector<ll>(n + 1, 0));
     for (int i = 1; i <= n; ++i) {
         for (int j = 1; j <= n; ++j) {
             cin >> mp[i][j];
         }
+    }
+    for (int i = 1; i <= n; ++i) {
+        for (int j = 1; j <= n; ++j) {
+            mp[i][j] += mp[i][j - 1];
+        }
+    }
+    subtree(1, n);
+    // cout << fro[1][n] << " ";
+    build(1, n, 0);
+    for (int i = 1; i <= n; ++i) {
+        cout << fa[i] << " ";
     }
 }
 auto main() -> int {
