e4e707ab6eb74cf3a76b1392ac94fc0ebe48f854
siyang shao <siyang.shao@outlook.com>
2022-05-20 16:58:46 +0800

    greedy

--- codeforces/R792/D.cpp
+++ codeforces/R792/D.cpp
@@ -4,20 +4,19 @@ using ll = long long;
 auto solve() {
     ll n, k;
     cin >> n >> k;
-    vector<ll> a(n + 1), dp(n + 1);
-    a.assign(n + 1, 0), dp.assign(n + 1, 0);
-    for (ll i = n; i >= 1; --i) {
+    vector<ll> a(n);
+    ll ans = 0;
+    for (ll i = 0; i < n; ++i) {
         cin >> a[i];
+        ans += a[i];
+        a[i] = a[i] - (n - 1 - i);
     }
-    dp[0] = 0;
-    for (ll i = 1; i <= n; ++i) {
-        dp[i] = 0;
-        for (ll jump = max(k, i - 1); jump >= 1; --jump) {
-            dp[jump] = min(dp[jump] + a[i], dp[jump - 1] + (i - jump));
-        }
-        dp[0] += a[i];
+    sort(a.begin(), a.end(), greater<>());
+    for (ll i = 0; i < k; ++i) {
+        ans -= a[i];
     }
-    cout << dp[k] << "\n";
+    ans -= (k) * (k - 1) / 2;
+    cout << ans << "\n";
 }
 auto main() -> int {
     ios::sync_with_stdio(false);
