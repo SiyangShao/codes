2dc038b442dfd1b3c7989d98df006ed0481611c3
siyang shao <siyang.shao@outlook.com>
2022-05-22 12:55:19 +0800

    Dis_Max not Large enough

---
+++ AtCoder/ABC252/A.cpp
@@ -0,0 +1,17 @@
+#include <bits/stdc++.h>
+using namespace std;
+using ll = long long;
+auto solve() {
+    int n;
+    cin >> n;
+    cout << char(n) << "\n";
+}
+auto main() -> int {
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr), cout.tie(nullptr);
+    int _ = 1;
+    // cin >> _;
+    while (_--) {
+        solve();
+    }
+}
