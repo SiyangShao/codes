6fb427e7708c84d3f0e78266cd3b7014002c7265
siyang shao <siyang.shao@outlook.com>
2022-05-25 18:55:20 +0800

    懒得写了

---
+++ ICPC/training/HZNU22/J.cpp
@@ -0,0 +1,18 @@
+#include <bits/stdc++.h>
+using namespace std;
+using ll = long long;
+constexpr ll N = 1e5 + 7;
+auto solve() {
+    ll q;
+    cin >> q;
+    
+}
+auto main() -> int {
+    // ios::sync_with_stdio(false);
+    // cin.tie(nullptr), cout.tie(nullptr);
+    ll _ = 1;
+    // cin >> _;
+    while (_--) {
+        solve();
+    }
+}
